a7a0dabb608d860c407c4f6a606406a8e2259d2c
FURB\jdomareski <jefferson.domareski@gmail.com>
2022-10-25 20:14:16 -0300

    Ajustes

---
+++ Jefferson/Segunda Versao/Project3.dpr
@@ -0,0 +1,15 @@
+program Project3;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.pas' {Form2},
+  UCaixa in 'UCaixa.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
